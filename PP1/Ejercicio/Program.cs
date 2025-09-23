58eace02bdf6510ba8eff671de79c24200f6019d
Sequeira Fernandez Johannes Armando <84215987+JohannesSeq@users.noreply.github.com>
2025-09-22 20:41:17 -0600

    empezando tarea #1

---
+++ PP1/Ejercicio/Program.cs
@@ -0,0 +1,26 @@
+﻿//Metodo #1 Validar asendentemente desde 1 hasta Max
+int sumFor(int n)
+{
+    int sum = 0;
+    int Max = 2147483647;
+
+    for (int i = 0; i < Max; i++)
+    {
+
+
+
+    }
+
+    return sum;
+
+}
+
+//Metodo #2 Validar desendentemente desde Max hasta 1
+int sumIte (int n)
+{
+
+    int result = 0;
+    return result;
+
+}
+
