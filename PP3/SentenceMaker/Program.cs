da4cb3a4549c293f42bee2e4765df7b7249c76f1
Sequeira Fernandez Johannes Armando <84215987+JohannesSeq@users.noreply.github.com>
2025-10-20 18:16:54 -0600

    Avanzando tarea 3

--- PP3/SentenceMaker/Program.cs
+++ PP3/SentenceMaker/Program.cs
@@ -7,6 +7,9 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+//Variable de oración la cual va a ser utilizada por el Api
+string oracion = "This is just a test that shows how the 'include' endpoint works over some text of this sentence";
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -14,27 +17,56 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+// Endpoint: /
+//Endpoint encargado de la redireccion a la documentacion del API en Swagger.
+app.MapGet("/", () => Results.Redirect("/swagger/index.html"));
+
+
+//Endpoint de Get solamente para probar el estado de la oracion, voy a ser eliminado en las futuras versiones
+app.MapGet("/read", () => oracion);
+
+//Endpoint: /include
+//Endpoint encargado de incluir una nueva palabra a la oracion. Recibe dos parametros, la palabra y el indice donde vamos a colocar la misma.
+app.MapPost("/include/{index:int}", (int index, string value) =>
+{
+    String oracionPrev = oracion;
+
+    //Validaciones de los datos ingresados
+    if (value == "")
+    {
+        return Results.BadRequest("'value' cannot be empty");
+    }
+    else if (index < 0)
+    {
+        return Results.BadRequest("'position' must be 0 or higher");
+
+    } else if (index > oracion.Length)
+    {
+        return Results.BadRequest("'position' must not exceed current sentence lenght");
+    }
+    else
+    {
+        try
+        {
+            oracion = oracion.Insert(index, value);
+
+            return Results.Ok(oracion);
+
+        }
+        catch
+        {
+            return Results.BadRequest("Unexpected error handling your request.");
+
+        }
+
+    }
+
+});
+
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+
 
 app.Run();
 
